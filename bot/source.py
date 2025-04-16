61dd6110639877f9bcfdb77a6b6dacf1f6b55f01
mnasibzade <euoryexe@gmail.com>
2025-04-16 13:13:44 +0400

    bot: Add source code of bot and adjust message - Now it is more user-friendly
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- bot/source.py
+++ bot/source.py
@@ -1,11 +1,11 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-
 async def source(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("Source code of AlphaDroid", url="https://github.com/alphadroid-project")],
-        [InlineKeyboardButton("Source of OFFICIAL devices", url="https://github.com/AlphaDroid-devices")]
+        [InlineKeyboardButton("AlphaDroid Source Code", url="https://github.com/alphadroid-project")],
+        [InlineKeyboardButton("Official Devices Resources", url="https://github.com/AlphaDroid-devices")],
+        [InlineKeyboardButton("Bot Source Code", url="https://github.com/mnasibzade/AlphaDroidHelp_Bot")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Here is the AlphaDroid Source Code!', reply_markup=reply_markup)
+    await update.message.reply_text('Looking for the source? Here you go! 👇', reply_markup=reply_markup)
