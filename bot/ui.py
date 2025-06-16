00e28845ecf3f6e8febd4fa692f42751acd84aca
Munir Nasibzade <euoryexe@gmail.com>
2025-06-16 09:08:22 +0400

    bot: Update 3.X screenshots

--- bot/ui.py
+++ bot/ui.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, CallbackContext
 
 async def ui(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("UI references from 3.X", url="https://t.me/alphadroid_chat/63893")],
+        [InlineKeyboardButton("UI references from 3.X", url="https://t.me/alphadroid_releases/478")],
         [InlineKeyboardButton("2.X", url="https://t.me/alphadroid_releases/363"), InlineKeyboardButton("1.X", url="https://t.me/alphadroid_releases/154")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
