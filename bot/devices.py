0acecf4943e7e7c52233a3ad482f6b9c23a3ce2c
Munir Nasibzade <euoryexe@gmail.com>
2025-12-21 03:55:32 +0400

    devices: Update URL for official devices list
    - now we have a domain

--- bot/devices.py
+++ bot/devices.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, CallbackContext
 
 async def devices(update: Update, context: CallbackContext) -> None:
     keyboard = [
-        [InlineKeyboardButton("List of OFFICIAL devices", url="https://alphadroid-project.github.io/devices.html")],
+        [InlineKeyboardButton("List of OFFICIAL devices", url="https://alphadroid.org/#devices")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text('You can find out whether your device is officially supported by us by clicking the button below.', reply_markup=reply_markup)
